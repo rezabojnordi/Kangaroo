8cc734b8d84c285332aff0ecf48aac87f310860d
rezabojnordi <rezabojnordi2012@gmail.com>
2022-01-20 13:26:18 +0330

    track process

--- master/api/src/utils/track_process.py
+++ master/api/src/utils/track_process.py
@@ -1,1 +1,28 @@
 # To do process
+
+
+
+
+class TrackProcess():
+    def __init__(self,data):
+        self.data = data
+    
+
+
+
+
+    def get_from_db(self):
+        pass
+
+
+
+    def save_from_db(self):
+        pass
+
+
+    def delete_from_db(self):
+        pass
+
+
+
+
